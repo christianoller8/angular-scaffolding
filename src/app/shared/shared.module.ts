6feb7d97fe87a8b423775af7107a82b9a4075525
Christian Oller <christianoller.dev@gmail.com>
2023-05-09 11:05:00 +0200

    General configurations, routing and lazy loading added

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
-import { MaterialModule } from "./components/material/material.module";
+import { MaterialModule } from "./material/material.module";
 
 @NgModule({
   declarations: [],
   imports: [CommonModule, MaterialModule],
+  exports: [MaterialModule],
 })
 export class SharedModule {}
